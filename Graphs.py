4ce34abed83cf4504878b727356199383a6d8bd3
Ishaan Agarwal <73911978+IshaanA-projects@users.noreply.github.com>
2025-09-30 22:53:37 +0100

    Graphs.py

--- Graphs.py
+++ Graphs.py
@@ -27,43 +27,38 @@ rotate_x_linear = x_rotate(x, y, theta)
 rotate_x_quadratic =  x_rotate(x, y_quadratic, theta)
 rotate_x_cubic = x_rotate(x, y_cubic, theta)
 
+
+
 plt.close("all")
+plt.subplot(3, 2, 1)
+plt.plot(x, y)
+plt.grid("all")
+plt.title("Linear graph")
 
-# Make figure square and use square subplots
-fig, axes = plt.subplots(3, 2, 
-                         figsize=(8, 12),      # overall figure size
-                         constrained_layout=True)  # automatically adjusts spacing
+plt.subplot(3, 2, 2)
+plt.plot(rotate_x_linear, rotate_y_linear)
+plt.grid("all")
+plt.title("Rotated linear graph")
 
-# Flatten axes array for easy looping
-axes = axes.flatten()
+plt.subplot(3, 2, 3)
+plt.plot(x, y_quadratic)
+plt.grid("all")
 
-# Original graphs
-axes[0].plot(x, y)
-axes[0].grid("all")
-axes[0].set_title("Linear graph")
+plt.title("Quadratic graph")
 
-axes[1].plot(rotate_x_linear, rotate_y_linear)
-axes[1].grid("all")
-axes[1].set_title("Rotated linear graph")
+plt.subplot(3, 2, 4)
+plt.plot(rotate_x_quadratic, rotate_y_quadratic)
+plt.grid("all")
+plt.title("Rotated Quadratic graph")
 
-axes[2].plot(x, y_quadratic)
-axes[2].grid("all")
-axes[2].set_title("Quadratic graph")
+plt.subplot(3, 2, 5)
+plt.plot(x, y_cubic)
+plt.grid("all")
+plt.title("Cubic graph")
 
-axes[3].plot(rotate_x_quadratic, rotate_y_quadratic)
-axes[3].grid("all")
-axes[3].set_title("Rotated Quadratic graph")
+plt.subplot(3, 2, 6)
+plt.plot(rotate_x_cubic, rotate_y_cubic)
+plt.grid("all")
+plt.title("Rotated cubic graph")
 
-axes[4].plot(x, y_cubic)
-axes[4].grid("all")
-axes[4].set_title("Cubic graph")
-
-axes[5].plot(rotate_x_cubic, rotate_y_cubic)
-axes[5].grid("all")
-axes[5].set_title("Rotated cubic graph")
-
-# Make each subplot square
-for ax in axes:
-    ax.set_aspect('equal', adjustable='box')
-
-plt.show()
+plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9, hspace=0.7)
